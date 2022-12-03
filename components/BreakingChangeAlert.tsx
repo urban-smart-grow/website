c9a3305e2c3e2651f9f3e30844f95ab70610374e
Malte Fillmer <malte.fillmer@gmail.com>
2022-12-03 22:18:46 +0000

    add styles

--- components/BreakingChangeAlert.tsx
+++ components/BreakingChangeAlert.tsx
@@ -1,5 +1,7 @@
 import React from 'react'
 
 export const BreakingChangeAlert = () => (
-  <strong>This is subject to redesign in the future!</strong>
+  <strong className='text-red-400 block my-4'>
+    This is subject to redesign in the future!
+  </strong>
 )

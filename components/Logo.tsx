1684407151049fb9c7e68e2beac9abfe1a780e75
Malte Fillmer <malte.fillmer@gmail.com>
2022-12-13 20:05:17 +0000

    fix warnings

--- components/Logo.tsx
+++ components/Logo.tsx
@@ -1,24 +1,25 @@
-export const Logo = () => (<svg
-  xmlns='http://www.w3.org/2000/svg'
-  viewBox='0 0 64 64'
-  width='64'
-  height='64'>
-  <g stroke='black' fill-opacity='0' stroke-linecap='round'>
-    <circle r='16' cx='32' cy='26' />
-    <path d='M 24 44 A 16 16 0 0 0 40 44' />
-    <path d='M 24 48 A 16 16 0 0 0 40 48' />
-    <path d='M 24 52 A 16 16 0 0 0 40 52' />
-    <path d='M 24 56 A 16 16 0 0 0 40 56' />
-  </g>
-  <g
-    stroke='black'
-    fill-opacity='0'
-    stroke-linejoin='round'
-    stroke-linecap='round'
-    stroke-width='0.5'>
-    <path d='M 20 22 A 11.66 11.66 0 0 0 44 22' />
-    <path d='M 20 22 A 11.66 11.66 0 0 1 29 41.5' />
-    <path d='M 44 22 A 11.66 11.66 0 0 0 35 41.5' />
-  </g>
-</svg>
+export const Logo = () => (
+  <svg
+    xmlns='http://www.w3.org/2000/svg'
+    viewBox='0 0 64 64'
+    width='64'
+    height='64'>
+    <g stroke='black' fillOpacity='0' strokeLinecap='round'>
+      <circle r='16' cx='32' cy='26' />
+      <path d='M 24 44 A 16 16 0 0 0 40 44' />
+      <path d='M 24 48 A 16 16 0 0 0 40 48' />
+      <path d='M 24 52 A 16 16 0 0 0 40 52' />
+      <path d='M 24 56 A 16 16 0 0 0 40 56' />
+    </g>
+    <g
+      stroke='black'
+      fill-opacity='0'
+      strokeLinejoin='round'
+      strokeLinecap='round'
+      strokeWidth='0.5'>
+      <path d='M 20 22 A 11.66 11.66 0 0 0 44 22' />
+      <path d='M 20 22 A 11.66 11.66 0 0 1 29 41.5' />
+      <path d='M 44 22 A 11.66 11.66 0 0 0 35 41.5' />
+    </g>
+  </svg>
 )

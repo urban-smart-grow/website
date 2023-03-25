1f32c11e5386044270077ae0b21f88b15e275fec
Malte Fillmer <malte.fillmer@gmail.com>
2023-03-25 10:35:19 +0000

    add cookie banner

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,5 +3,14 @@ import 'nextra-theme-docs/style.css'
 import type { AppProps } from 'next/app'
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Component {...pageProps} />
+      <script
+        type='text/javascript'
+        async
+        id='cookiebanner'
+        src='https://cdn.jsdelivr.net/gh/dobarkod/cookie-banner@1.2.2/dist/cookiebanner.min.js'></script>
+    </>
+  )
 }

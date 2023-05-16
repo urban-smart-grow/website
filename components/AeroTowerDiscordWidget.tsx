6c2c77d06dbf559544f61b724a1019ff01b22837
Malte Fillmer <12849528+mfillmer@users.noreply.github.com>
2023-05-16 20:41:42 +0000

    remove stale import

--- components/AeroTowerDiscordWidget.tsx
+++ components/AeroTowerDiscordWidget.tsx
@@ -1,8 +1,6 @@
-import WidgetBot from "@widgetbot/react-embed";
-
 export const AeroTowerDiscordWidget = () => (
   <iframe
     src="https://e.widgetbot.io/channels/1107347850809053184/1108118045223813232"
-    className="w-full h-[50vh]"
+    className="w-full h-[66vh]"
   ></iframe>
  );

de00aa10604c3f007c0a4b6bf9b73384a739ec7e
Malte Fillmer <12849528+mfillmer@users.noreply.github.com>
2023-05-16 20:36:33 +0000

    add discord widget for comments

---
+++ components/AeroTowerDiscordWidget.tsx
@@ -0,0 +1,8 @@
+import WidgetBot from "@widgetbot/react-embed";
+
+export const AeroTowerDiscordWidget = () => (
+  <iframe
+    src="https://e.widgetbot.io/channels/1107347850809053184/1108118045223813232"
+    className="w-full h-[50vh]"
+  ></iframe>
+ );

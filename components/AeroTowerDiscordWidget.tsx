0f3098c2e37b2e50915ff9be1cdfc351491c685f
Malte Fillmer <12849528+mfillmer@users.noreply.github.com>
2023-05-22 18:25:19 +0000

    add discord button and separate channels for every page

--- components/AeroTowerDiscordWidget.tsx
+++ components/AeroTowerDiscordWidget.tsx
@@ -1,6 +1,27 @@
-export const AeroTowerDiscordWidget = () => (
-  <iframe
-    src="https://e.widgetbot.io/channels/1107347850809053184/1108118045223813232"
-    className="w-full h-[66vh]"
-  ></iframe>
- );
+import { DiscordButton } from "./DiscordButton";
+
+export const aerotower_model = "1108338231600238705";
+export const aerotower_comments = "1108118045223813232";
+export const aerotower_electronics = "1108338337086980127";
+
+const discord_server_id = "1107347850809053184";
+
+interface Props {
+  server?: string | number;
+  channel?: string | number;
+}
+
+export const AeroTowerDiscordWidget = ({
+  server = discord_server_id,
+  channel = aerotower_comments,
+}: Props) => {
+  return (
+    <>
+      <iframe
+        src={`https://e.widgetbot.io/channels/${server}/${channel}`}
+        className="w-full h-[66vh]"
+      />
+      <DiscordButton/>
+    </>
+  );
+};

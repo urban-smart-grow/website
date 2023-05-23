931dfe8a6c7eb80a70d456267297426298c78ff6
malte <malte.fillmer@gmail.com>
2023-05-23 20:30:33 +0200

    describe solar module

--- components/AeroTowerDiscordWidget.tsx
+++ components/AeroTowerDiscordWidget.tsx
@@ -1,14 +1,15 @@
-import { DiscordButton } from "./DiscordButton";
+import { DiscordButton } from './DiscordButton'
 
-export const aerotower_model = "1108338231600238705";
-export const aerotower_comments = "1108118045223813232";
-export const aerotower_electronics = "1108338337086980127";
+export const aerotower_model = '1108338231600238705'
+export const aerotower_comments = '1108118045223813232'
+export const aerotower_electronics = '1108338337086980127'
+export const solar_module = '1110634927315238946'
 
-const discord_server_id = "1107347850809053184";
+const discord_server_id = '1107347850809053184'
 
 interface Props {
-  server?: string | number;
-  channel?: string | number;
+  server?: string | number
+  channel?: string | number
 }
 
 export const AeroTowerDiscordWidget = ({
@@ -19,9 +20,9 @@ export const AeroTowerDiscordWidget = ({
     <>
       <iframe
         src={`https://e.widgetbot.io/channels/${server}/${channel}`}
-        className="w-full h-[66vh]"
+        className='w-full h-[66vh]'
       />
-      <DiscordButton/>
+      <DiscordButton />
     </>
-  );
-};
+  )
+}
